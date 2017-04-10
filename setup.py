abc6f0d6a09e47e29e588055aef92689b3730651
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-04-10 16:01:50 -0300

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='Pycards',
-    version='1.0.0',
+    version='1.1',
     url='https://github.com/mrlucascardoso/pycards',
     license='MIT License',
     author='Lucas Cardoso',
